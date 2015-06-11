6d8e2f240586efb4a8b9dd65140ead13b481570a
Mathias Schaemelhout <mathias.schaemelhout@digipolis.be>
2015-06-11 14:08:47 +0200

    no message

---
+++ dist/awelzijn-mail-to.js
@@ -0,0 +1,19 @@
+'use strict';
+(function (module) {
+  try {
+    module = angular.module('awelzijn.mail-to');
+  } catch (e) {
+    module = angular.module('awelzijn.mail-to', []);
+  }
+  module.directive('aWelzijnMailTo', [function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      template: '<a href="mailto:{{emailAdres}}">{{emailAdres}}</a>',
+      scope: {
+        emailAdres: "="
+      }
+    }
+  }]);
+})();
+;
