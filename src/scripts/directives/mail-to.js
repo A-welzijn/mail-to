25e07b2844e305d31574ab668200de17f9c27f99
Schaemelhout <Mathias015@hotmail.com>
2015-09-29 10:32:31 +0200

    no message

--- src/scripts/directives/mail-to.js
+++ src/scripts/directives/mail-to.js
@@ -9,9 +9,12 @@
     return {
       restrict: 'E',
       replace: true,
-      template: '<a href="mailto:{{emailAdres}}">{{emailAdres}}</a>',
+      template: '<a ng-show="emailAdres" href="mailto:{{emailAdres}}">{{emailAdres}}</a><span ng-show="!emailAdres">-</span>',
       scope: {
         emailAdres: "="
+      },
+			link: function (scope, element, attrs) {
+        
       }
     }
   }]);
